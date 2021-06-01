2daff7894255afa08afb104d4ee2fc84cc1ada41
wowococo <1695599369@qq.com>
2021-06-02 06:52:59 +0800

    update

--- tomato.go
+++ tomato.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"os"
 	"time"
-	"termbox-go"
+
+	"github.com/nsf/termbox-go"
 )
 
 var usage = ` tomato usage:
@@ -12,27 +13,52 @@ tomato 25s
 tomato 1h20m20s
 `
 
+var (
+	timer  *time.Timer
+	ticker *time.Ticker
+)
+
+const (
+	tick = time.Second
+)
+
+func start(d time.Duration) {
+	timer = time.NewTimer(d)
+	ticker = time.NewTicker(tick)
+}
+
+func stop() {
+	timer.Stop()
+	ticker.Stop()
+}
+
+func draw() {
+
+}
+
 func countdown(d time.Duration) {
 	timeleft := d
-	
-	loop:
-		for {
-			select {
-				case ev <- queues:
-					if ev.Type == EventKey and (ev.Key == KeyCtrlC or ev.Key == KeyEsc) {
-						break loop
-					}
-					if ev.Ch == "P" or ev.Ch == 'p':
-						stop()
-					if ev.Ch == 'C' or ev.Ch == 'c':
-						start()
-				case <- ticker.C:
-					draw()
-				case <- timer.C:
-					break loop
-			}
 
+loop:
+	for {
+		select {
+		case ev := <-queues:
+			if ev.Type == termbox.EventKey && (ev.Key == termbox.KeyCtrlC || ev.Key == termbox.KeyEsc) {
+				break loop
+			}
+			if ev.Ch == "P" || ev.Ch == 'p' {
+				stop()
+			}
+			if ev.Ch == 'C' || ev.Ch == 'c' {
+				start(d)
+			}
+		case <-ticker.C:
+			draw()
+		case <-timer.C:
+			break loop
 		}
+
+	}
 }
 
 func main() {
