f1b995d91a99e37e2fe405e0a1faa705db889b6a
wowococo <1695599369@qq.com>
2021-07-01 08:02:52 +0800

    remove commit

--- stats/handler.go
+++ stats/handler.go
@@ -5,7 +5,6 @@ import (
 	_ "log"
 	_ "tomato-clock/sqliteopt"
 
-	_ "github.com/gizak/termui/v3"
 	"github.com/mum4k/termdash"
 	"github.com/mum4k/termdash/align"
 	"github.com/mum4k/termdash/cell"
@@ -18,73 +17,6 @@ import (
 	"github.com/mum4k/termdash/widgets/linechart"
 	"github.com/mum4k/termdash/widgets/text"
 )
-
-// func drawText() {
-// 	p0 := widgets.NewParagraph()
-// 	p0.Title = "总完成番茄数"
-// 	p0.Text = sqliteopt.Query("tomato", "progress", "total")
-// 	p0.TextStyle = ui.NewStyle(ui.ColorRed)
-// 	p0.SetRect(0, 0, 15, 3)
-// 	p0.Border = true
-
-// 	p1 := widgets.NewParagraph()
-// 	p1.Title = "本周完成番茄数"
-// 	p1.Text = sqliteopt.Query("tomato", "progress", "thisweek")
-// 	p1.TextStyle = ui.NewStyle(ui.ColorRed)
-// 	p1.SetRect(15, 0, 31, 3)
-// 	p1.Border = true
-
-// 	p2 := widgets.NewParagraph()
-// 	p2.Title = "今日完成番茄数"
-// 	p2.Text = sqliteopt.Query("tomato", "progress", "today")
-// 	p2.TextStyle = ui.NewStyle(ui.ColorRed)
-// 	p2.SetRect(32, 0, 48, 3)
-// 	p2.Border = true
-
-// 	p3 := widgets.NewParagraph()
-// 	p3.Title = "总专注时间"
-// 	p3.Text = sqliteopt.Query("tomato", "timefocused", "total")
-// 	p3.TextStyle = ui.NewStyle(ui.ColorRed)
-// 	p3.SetRect(0, 3, 15, 6)
-// 	p3.Border = true
-
-// 	p4 := widgets.NewParagraph()
-// 	p4.Title = "本周专注时间"
-// 	p4.Text = sqliteopt.Query("tomato", "timefocused", "thisweek")
-// 	p4.SetRect(15, 3, 31, 6)
-// 	p4.TextStyle = ui.NewStyle(ui.ColorRed)
-// 	p4.Border = true
-
-// 	p5 := widgets.NewParagraph()
-// 	p5.Title = "今日专注时间"
-// 	p5.Text = sqliteopt.Query("tomato", "timefocused", "today")
-// 	p5.TextStyle = ui.NewStyle(ui.ColorRed)
-// 	p5.SetRect(32, 3, 48, 6)
-// 	p5.Border = true
-
-// 	p6 := widgets.NewParagraph()
-// 	p6.Title = "总完成任务"
-// 	p6.Text = sqliteopt.Query("task", "id", "total")
-// 	p6.TextStyle = ui.NewStyle(ui.ColorRed)
-// 	p6.SetRect(0, 6, 15, 9)
-// 	p6.Border = true
-
-// 	p7 := widgets.NewParagraph()
-// 	p7.Title = "本周完成任务"
-// 	p7.Text = sqliteopt.Query("task", "id", "thisweek")
-// 	p7.TextStyle = ui.NewStyle(ui.ColorRed)
-// 	p7.SetRect(15, 6, 31, 9)
-// 	p7.Border = true
-
-// 	p8 := widgets.NewParagraph()
-// 	p8.Title = "今日完成任务"
-// 	p8.Text = sqliteopt.Query("task", "id", "today")
-// 	p8.TextStyle = ui.NewStyle(ui.ColorRed)
-// 	p8.SetRect(32, 6, 48, 9)
-// 	p8.Border = true
-
-// 	ui.Render(p0, p1, p2, p3, p4, p5, p6, p7, p8)
-// }
 
 type widgets struct {
 	lc     *linechart.LineChart
