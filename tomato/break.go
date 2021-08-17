e2ed96ec1bc22653ed7abe3286926b62e6fa5b1c
wowococo <1695599369@qq.com>
2021-08-17 23:00:29 +0800

    Zancun，but has bug

--- tomato/break.go
+++ tomato/break.go
@@ -1,11 +1,17 @@
 package tomato
 
 import (
+	"context"
+	"image"
 	"log"
 	"time"
 
-	ui "github.com/gizak/termui/v3"
-	"github.com/gizak/termui/v3/widgets"
+	"github.com/mum4k/termdash"
+	"github.com/mum4k/termdash/align"
+	"github.com/mum4k/termdash/container"
+	"github.com/mum4k/termdash/terminal/tcell"
+	"github.com/mum4k/termdash/terminal/terminalapi"
+	"github.com/mum4k/termdash/widgets/text"
 )
 
 func breaktime(d time.Duration, tomatoID int64) {
@@ -16,18 +22,42 @@ func breaktime(d time.Duration, tomatoID int64) {
 }
 
 func echo() {
-	err := ui.Init()
+	t, err := tcell.New()
 	if err != nil {
 		log.Fatalf("failed to initialize termui: %v", err)
 	}
-	defer ui.Close()
-	p := widgets.NewParagraph()
+	defer t.Close()
 
-	p.Text = "TIME TO BREAK ^_^ "
+	_, cancel := context.WithCancel(context.Background())
+	borderless, err := text.New()
+	if err != nil {
+		panic(err)
+	}
+	borderless.Options().MaximumSize = image.Point{X: 10, Y: 10}
 
-	p.SetRect(0, 0, 25, 5)
+	if err := borderless.Write("TIME TO BREAK ^_^ "); err != nil {
+		panic(err)
+	}
 
-	ui.Render(p)
+	c, err := container.New(t,
+		container.PlaceWidget(borderless),
+		container.AlignHorizontal(align.HorizontalCenter),
+		container.AlignVertical(align.VerticalMiddle))
+	if err != nil {
+		panic(err)
+	}
+	quitter := func(k *terminalapi.Keyboard) {
+		if k.Key == 'q' || k.Key == 'Q' {
+			cancel()
+		}
+	}
+
+	controller, err := termdash.NewController(t, c, termdash.KeyboardSubscriber(quitter))
+	if err != nil {
+		panic(err)
+	}
+	defer controller.Close()
+
 	time.Sleep(2 * time.Second)
 
 }
